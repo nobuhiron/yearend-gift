3cae5e48a718a0dd3f05574bbd233ec52ba9f646
Nobuhiro Naito <n-naito@kyoeiseicha.co.jp>
2025-10-21 18:31:56 +0900

    修正

--- src/data/sweetsAssortmensItemData.js
+++ src/data/sweetsAssortmensItemData.js
@@ -20,11 +20,4 @@ export const sweetsAssortmensItemData = [
       '波型サクサク食感のクッキー生地に、上品な味わいの抹茶クリームをサンドしました。',
     price: '1,954',
   },
-  {
-    imgUrl: 'price-item_01.jpg',
-    title: '森半 クリームサンド 抹茶ヴァッフェル詰合せ [10個入り]',
-    description:
-      '波型サクサク食感のクッキー生地に、上品な味わいの抹茶クリームをサンドしました。',
-    price: '1,954',
-  },
 ];

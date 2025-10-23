10a72e3f1fbeb6929085d52d3d657d3b69c4a575
nobuhiro <n.naito1014@gmail.com>
2025-10-24 07:26:17 +0900

    修正

---
+++ src/data/rankingItemData.js
@@ -0,0 +1,27 @@
+export const rankingItemData = [
+  {
+    index: 1,
+    title: '<宇治>名品銘茶 詰め合わせ(高山寺)',
+    price: '5,400',
+  },
+  {
+    index: 1,
+    title: 'お茶屋のスイーツ満足セット',
+    price: '5,100',
+  },
+  {
+    index: 3,
+    title: '京の現代名工・吉田利一作 <br>宇治玉露 [100g缶]',
+    price: '5,400',
+  },
+  {
+    index: 4,
+    title: '銘茶五香(ごこう)・<br>抹茶カステラ詰合せ<br>￥4,542（税込）',
+    price: '4,542',
+  },
+  {
+    index: 5,
+    title: 'CHASTA(チャスタ)・ISSEN[10P入り] ギフトセット',
+    price: '5,390',
+  },
+];

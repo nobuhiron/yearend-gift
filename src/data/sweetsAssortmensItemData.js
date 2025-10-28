db867b739380ee237f33144d501dd2666b1f5462
Nobuhiro Naito <n-naito@kyoeiseicha.co.jp>
2025-10-28 14:16:38 +0900

    修正

--- src/data/sweetsAssortmensItemData.js
+++ src/data/sweetsAssortmensItemData.js
@@ -1,23 +1,25 @@
 export const sweetsAssortmensItemData = [
   {
-    imgUrl: 'price-item_01.jpg',
+    fileName: 'items/item-assortments-1.jpg',
+    code: '000000000186',
     title: '森半 クリームサンド 抹茶ヴァッフェル詰合せ [10個入り]',
     description:
       '波型サクサク食感のクッキー生地に、上品な味わいの抹茶クリームをサンドしました。',
     price: '1,954',
   },
   {
-    imgUrl: 'price-item_01.jpg',
-    title: '森半 クリームサンド 抹茶ヴァッフェル詰合せ [10個入り]',
-    description:
-      '波型サクサク食感のクッキー生地に、上品な味わいの抹茶クリームをサンドしました。',
+    fileName: 'items/item-assortments-2.jpg',
+    code: '000000000173',
+    title: '森半 クリームサンド ヴァッフェル(抹茶・ほうじ茶)詰合せ [各5個入り]',
+    description: 'お子様からお年を召した方まで、喜ばれる一品です。',
     price: '1,954',
   },
   {
-    imgUrl: 'price-item_01.jpg',
-    title: '森半 クリームサンド 抹茶ヴァッフェル詰合せ [10個入り]',
+    fileName: 'items/item-assortments-3.jpg',
+    code: '000000000183',
+    title: '森半 抹茶フィナンシェ・抹茶ヴァッフェル詰合せ [各4個入り]',
     description:
-      '波型サクサク食感のクッキー生地に、上品な味わいの抹茶クリームをサンドしました。',
-    price: '1,954',
+      'ちょっとした手土産にピッタリの「抹茶ヴァッフェル」「抹茶フィナンシェ」の詰め合わせギフトです。',
+    price: '1,576',
   },
 ];
--- src/data/sweetsOtherItemData.js
+++ src/data/sweetsOtherItemData.js
@@ -1,23 +1,26 @@
 export const sweetsOtherItemData = [
   {
-    imgUrl: 'price-item_01.jpg',
-    title: '森半 クリームサンド 抹茶ヴァッフェル詰合せ [10個入り]',
+    fileName: 'items/item-others-1.jpg',
+    code: '000000000168',
+    title: '森半 薄焼きロールクレープ 抹茶くるる [20本入り]',
     description:
-      '波型サクサク食感のクッキー生地に、上品な味わいの抹茶クリームをサンドしました。',
+      '薄焼きクレープを丸めて生地にし、内側に抹茶チョコレートを流し込みました。',
+    price: '1,663',
+  },
+  {
+    fileName: 'items/item-others-2.jpg',
+    code: '000000000175',
+    title: '極上手作り宇治抹茶カステラ ［5切］',
+    description:
+      '上級の宇治抹茶をたっぷり使って熟練の職人の手で焼き上げた極上カステラです。',
     price: '1,954',
   },
   {
-    imgUrl: 'price-item_01.jpg',
-    title: '森半 クリームサンド 抹茶ヴァッフェル詰合せ [10個入り]',
+    fileName: 'items/item-others-3.jpg',
+    code: '000000000169',
+    title: '森半 抹茶ダックワーズ [40個入り]',
     description:
-      '波型サクサク食感のクッキー生地に、上品な味わいの抹茶クリームをサンドしました。',
-    price: '1,954',
-  },
-  {
-    imgUrl: 'price-item_01.jpg',
-    title: '森半 クリームサンド 抹茶ヴァッフェル詰合せ [10個入り]',
-    description:
-      '波型サクサク食感のクッキー生地に、上品な味わいの抹茶クリームをサンドしました。',
-    price: '1,954',
+      'ちょっとした手土産にピッタリの「抹茶ヴァッフェル」「抹茶フィナンシェ」の詰め合わせギフトです。',
+    price: '6,566',
   },
 ];
